784238e77ffb134af51f96b5607a27f6f4f9b400
boniol <71459482+boniol@users.noreply.github.com>
2025-04-16 09:41:27 +0200

    Update inference.py

--- inference.py
+++ inference.py
@@ -4,8 +4,12 @@ import librosa
 import soundfile as sf
 import os
 
-def separate_vocals(input_path, output_path):
+def separate_vocals(input_path):
     model_path = "models/UVR-MDX-NET-Inst_HQ_3.onnx"
+    output_dir = "output_audio"
+    filename = os.path.basename(input_path)
+    instrumental_path = os.path.join(output_dir, f"{filename}_instrumental.wav")
+
     session = onnxruntime.InferenceSession(model_path)
 
     audio, sr = librosa.load(input_path, sr=44100, mono=True)
@@ -15,4 +19,5 @@ def separate_vocals(input_path, output_path):
     ort_inputs = {session.get_inputs()[0].name: audio}
     ort_outs = session.run(None, ort_inputs)
 
-    sf.write(output_path, ort_outs[0][0], sr)
+    # Zapisz instrumental (zwykle output[0] to instrumental)
+    sf.write(instrumental_path, ort_outs[0][0], sr)
